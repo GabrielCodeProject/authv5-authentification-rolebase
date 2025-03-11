39a5d01f090686a1ed87254f2be3b529ef857d7b
Gabriel <gabrielprivermsg@gmail.com>
2025-03-11 17:19:56 -0400

    add logout button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Button } from "@/components/ui/button";
+import { LogoutButton } from "@/components/auth/logout-button";
+import { logout } from "../actions/logout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LogoutButton>logout</LogoutButton>
         {children}
       </body>
     </html>
